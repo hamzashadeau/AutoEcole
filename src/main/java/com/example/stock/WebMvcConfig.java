e0dd5b53dbcafb186c3ef167ff669213b5993626
hamzashadeau <60945657+hamzashadeau@users.noreply.github.com>
2020-09-19 12:15:46 +0100

    test15

--- src/main/java/com/example/stock/WebMvcConfig.java
+++ src/main/java/com/example/stock/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
       registry.addResourceHandler("/**/*")
-        .addResourceLocations("classpath:/static/pfe-projectHRManagement/")
+        .addResourceLocations("classpath:/static/")
         .resourceChain(true)
         .addResolver(new PathResourceResolver() {
             @Override
@@ -25,7 +25,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 Resource location) throws IOException {
                 Resource requestedResource = location.createRelative(resourcePath);
                 return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                : new ClassPathResource("/static/pfe-projectHRManagement/index.html");
+                : new ClassPathResource("/static/index.html");
             }
         });
     }
